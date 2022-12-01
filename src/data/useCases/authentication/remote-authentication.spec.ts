a478408329efbef124d5c9c4e49670b6aa2e79e4
Lucca <luccazvps@gmail.com>
2022-12-01 14:49:42 -0300

    refactor: move duplicated code to a factory helper method

--- src/data/useCases/authentication/remote-authentication.spec.ts
+++ src/data/useCases/authentication/remote-authentication.spec.ts
@@ -1,13 +1,25 @@
 import { RemoteAuthentication } from "./remote-authentication";
 import { HttpPostClientSpy } from "../../test/mock-http-client";
 describe("RemoteAuthentication", () => {
-  test("Should call HttpClient with correct url", async () => {
-    const url = "any_url";
+  interface SutTypes {
+    sut: RemoteAuthentication;
+    httpPostClientSpy: HttpPostClientSpy;
+  }
+  const makeSut = (): SutTypes => {
+    var url = "any_url";
     const httpPostClientSpy = new HttpPostClientSpy();
-    const spy = jest.spyOn(httpPostClientSpy, "post");
     const sut = new RemoteAuthentication(url, httpPostClientSpy);
+
+    return {
+      httpPostClientSpy,
+      sut,
+    };
+  };
+  test("Should call HttpClient with correct url", async () => {
+    const { httpPostClientSpy, sut } = makeSut();
+    const spy = jest.spyOn(httpPostClientSpy, "post");
     await sut.auth({ email: "any_email", password: "any_password" });
-    expect(sut.url).toBe(url);
-    expect(spy).toHaveBeenCalledWith(url);
+    expect(sut.url).toBe("any_url");
+    expect(spy).toHaveBeenCalledWith("any_url");
   });
 });
